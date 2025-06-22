eca882ecffa389a9c7541f0ce449fc73cc371195
bharathkumarp7736 <bharathkumarp7736@gmail.com>
2025-06-22 14:12:20 +0530

    success

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,28 @@
+package javabanking;
+class BankAccount{
+    String accountholdername;
+    long accountnumber;
+    double balance;
+
+    BankAccount(String name,long accnum){
+        this.accountholdername=name;
+        this.accountnumber=accnum;
+    }
+        void deposit(double amount){
+            balance=balance+amount;
+            System.out.println("amount successfully deposited :" + amount);
+        }
+        void withdraw(double amount){
+            if(amount<=balance){
+                balance=balance-amount;
+                System.out.println("amount withdraw successfuly :" + amount);
+            }
+            else{
+                System.out.println("insufficient balance :" + balance);
+            }
+        }
+        void checkbalance(){
+                System.out.println("balance :" + balance);
+            }
+    }
 
--- Main.java
+++ Main.java
@@ -1,1 +1,50 @@
+package javabanking;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the account holder name :");
+        String name = scan.nextLine();
+        System.out.println("enter your account number :");
+        long accnum = scan.nextLong();
+
+        BankAccount b1 = new BankAccount(name,accnum);
+
+        while(true){
+            System.out.println("--menu--");
+            System.out.println("1. deposit");
+            System.out.println("2. withdraw");
+            System.out.println("3. balance");
+            System.out.println("4. exit");
+            System.out.println("enter your choice :");
+
+            int choice=scan.nextInt();
+
+            if(choice==1){
+                System.out.println("enter amount to deposit :");
+                double amount=scan.nextDouble();
+                b1.deposit(amount);
+            }
+
+            else if(choice==2){
+                System.out.println("enter amount to withdraw :");
+                double amount=scan.nextDouble();
+                b1.withdraw(amount);
+            }
+
+            else if(choice==3){
+                System.out.println("account balance :");
+                b1.checkbalance();
+            }
+
+            else if(choice==4){
+                System.out.println("--thanks for using our bank--");
+                break;
+            }
+            else{
+                System.out.println("invalid number");
+            }
+        }
+    }
+}
 
